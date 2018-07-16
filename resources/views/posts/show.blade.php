639fa68cce993a5946a3986987263d19a2ec7dbb
MajaMarkovic1 <markovicm9000@gmail.com>
2018-07-16 21:54:48 +0200

    comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,10 +7,16 @@
 <div> {{$post->body}} </div> -->
 
 <div class="blog-post">
-    <h2 class="blog-post-title"><a href='/posts/{{$post->id}}'>{{ $post->title }}</a></h2>
+    <h2 class="blog-post-title">{{ $post->title }}</h2>
     <p class="blog-post-meta">{{$post->created_at}}</p>
     <p> {{$post->body}}</p>
 </div><!-- /.blog-post -->
+
+@foreach($post->comment as $comment)
+    <li>{{$comment->author}}</li>
+    <li>{{$comment->text}}</li>
+@endforeach
+    
 
 
 @endsection
